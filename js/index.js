80e7786dfd3ca103e7dc00b3c402524812adf6ac
billyRoyR <156974090+billyRoyR@users.noreply.github.com>
2024-06-16 08:20:13 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,4 @@
+let age = 10;
+
+let msg = (age >= 18) ? "You are an adult" : "You are a minor";
+console.log(msg);
---
+++ myobjects.js
@@ -0,0 +1,10 @@
+let myApiResponse = {
+    title: 'Title to my API',
+    status: 200,
+    isSuccess: true,
+    data: 'This is the data'
+}
+
+console.log(myApiResponse.data);
+
+console.log(`The data from API is - ${myApiResponse.data}`);
