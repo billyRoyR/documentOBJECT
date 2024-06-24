0813487cfa83623cc462f9ba17de184d598b6953
billyRoyR <156974090+billyRoyR@users.noreply.github.com>
2024-06-24 10:05:19 +0100

    micro-clone

---
+++ amazonClone/js/script.js
@@ -0,0 +1,24 @@
+let firstName = "Samuel";
+console.log(firstName);
+
+let favoriteFruit = "Strawberry";
+let favoriteIceCream = "Chocolate";
+let favoriteProgrammingLanguage = `JavaScript`;
+
+let numberOfDonuts = 12;
+let pi = 3.14;
+
+let veryLargeNumber = 547365846375n;
+
+let lovesCoding = true;
+
+let favoriteColor;
+
+favoriteFruit = null;
+
+const uniqueKey = Symbol();
+
+let course = {
+    name: "JavaScript for beginners",
+    hours: 3
+};
